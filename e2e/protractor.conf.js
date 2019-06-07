fbe0a4384840ccc4db68349368d473c49535ad75
JMD <josephm.diza@gmail.com>
2019-06-07 14:05:10 -0700

    Setup Travis

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -25,4 +25,15 @@ exports.config = {
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
+
+const config = require('./protractor.conf').config;
+
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--headless', '--no-sandbox']
+  }
+};
+
+exports.config = config;
 };
